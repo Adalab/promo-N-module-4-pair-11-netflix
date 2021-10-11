a2964e8573688745172bc4dbdae8e8123e7ece3d
Jenny <bienesjenny@gmail.com>
2021-10-11 11:09:34 +0200

    Add first exercise of Netflix

--- src/index.js
+++ src/index.js
@@ -11,3 +11,40 @@ const serverPort = 4000;
 server.listen(serverPort, () => {
   console.log(`Server listening at http://localhost:${serverPort}`);
 });
+
+// API
+server.get("/movies", (req, res) => {
+  console.log("Peticion a la ruta /");
+  res.send("<html><body><ul>Lista de Peliculas</ul></body></html>")
+})
+server.get("/movies", (req, res) => {
+  console.log("Peticion a la ruta GET /movies");
+  console.log(req.query);
+
+  const response = {
+    success: true,
+      movies: [
+        {
+          id: '1',
+          title: 'Gambita de dama',
+          gender: 'Drama',
+          image: 'https://via.placeholder.com/150'
+        },
+        {
+          id: '2',
+          title: 'Friends',
+          gender: 'Comedia',
+          image: 'https://via.placeholder.com/150'
+        }
+      ]
+  }
+  
+  const filterdata = response.movies.filter((movies) => movies.title === req.query.title);
+  res.json(filterdata);
+})
+
+//server.post("/movies", (req, res) => {
+  //res.json({ error: "No esta permitido" })
+//})
+
+
