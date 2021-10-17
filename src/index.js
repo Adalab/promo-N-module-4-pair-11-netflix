8a264d1b498342c19a535b5c820579505bff4661
Jenny <bienesjenny@gmail.com>
2021-10-18 00:12:31 +0200

    Created database 1

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const cors = require('cors');
+const Database = require("better-sqlite3"); 
+const db = new Database('./src/db/database.db', { 
+  verbose: console.log
+ })
 
 // create and config server
 const server = express();
@@ -31,32 +35,58 @@ server.post("/user/add", (req, res) => {
     result: "user created"
   });
 })
+
+server.post("/login", (req, res)=>{
+  const email = req.body.email; 
+  const pass = req.body.pass; 
+
+  if(!email || !pass){
+      res.sendStatus(404)
+  }else{
+  //1-declarar mi query
+  const query = db.prepare("SELECT email, pass FROM users WHERE email = 'mari@mail.com' AND pass = '456781' "); 
+  //2-ejecutar la query
+  const foundUser = query.get(email, pass);
+
+      if(foundUser !== undefined){
+          res.json({userId: foundUser.id}); 
+      }else{
+          res.json({error: "Usuario no encontrado "});
+      }
+  }
+})
+
 // API
 server.get("/movies", (req, res) => {
-  console.log("Peticion a la ruta GET /movies");
-  console.log(req.query);
+  //console.log("Peticion a la ruta GET /movies");
+  //console.log(req.query);
+  //1-Declarar mi query
+  const query = db.prepare("SELECT * FROM movies");
+  //2-Ejecutar mi query
+  const responseBD = query.all();
+  console.log(responseBD);
 
-  const response = {
-    success: true,
-    movies: [
-      {
-        id: '1',
-        title: 'Gambita de dama',
-        gender: 'Drama',
-        image: 'https://via.placeholder.com/150'
-      },
-      {
-        id: '2',
-        title: 'Friends',
-        gender: 'Comedia',
-        image: 'https://via.placeholder.com/150'
-      }
-    ]
-  }
+  //const response = {
+    //success: true,
+    //movies: [
+      //{
+        //id: '1',
+        //title: 'Gambita de dama',
+        //gender: 'Drama',
+        //image: 'https://via.placeholder.com/150'
+      //},
+      //{
+        //id: '2',
+        //title: 'Friends',
+        //gender: 'Comedia',
+        //image: 'https://via.placeholder.com/150'
+      //}
+    //]
+  //}
   // req.query.filter  ,  req.query.sort
   //const filterdata = response.movies.filter((movies) => movies.title === req.query.title);
   //res.json(filterdata);
-  res.json(response);
+  res.json(responseBD);
 })
 
 //server.post("/movies", (req, res) => {
